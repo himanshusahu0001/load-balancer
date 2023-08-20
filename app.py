8529633d160add166a352346e23f7046273a8b46
Himanshu Sahu <himanshu00738@gmail.com>
2023-08-20 16:49:53 +0530

    add commetns

--- app.py
+++ app.py
@@ -2,20 +2,23 @@ from flask import *
 import os
 import json
 import subprocess
-import requests
+import requests 
 
 app = Flask(__name__)
 CONFIG_FILE = 'config.json'
 
 def save_config(data):
+    # Save configuration data to the specified file
     with open(CONFIG_FILE, 'w') as f:
         json.dump(data, f)
 
 def load_config():
     if os.path.exists(CONFIG_FILE):
+        # Load configuration data from the file if it exists
         with open(CONFIG_FILE, 'r') as f:
             return json.load(f)
     else:
+        # Create initial configuration data if the file doesn't exist
         initial_data = {
             "number_of_workers": 3,
             "average_delay": 1,
@@ -36,16 +39,17 @@ class LoadBalancer:
         if not self.server_process:
             self.i = -1
             self.n = int(load_config()["number_of_workers"])
+            # Start worker servers as subprocesses
             self.server_process = subprocess.Popen(['python', 'workers.py'])
             print("worker servers started.")
 
     def stop_server(self):
         if self.server_process:
+            # Terminate the worker server subprocess
             self.server_process.kill()
             self.server_process.wait()
             self.server_process = None
             print("worker servers stopped.")
-
 
 load_balancer = LoadBalancer()
 
@@ -53,6 +57,7 @@ def round_robin_worker_call(endpoint):
     load_balancer.i =  (load_balancer.i+1) % load_balancer.n
     url = f"http://127.0.0.1:{5000 + load_balancer.i}"+endpoint
 
+    # Make a request to the selected worker server
     response = requests.get(url)
     api_response = Response(response.content, status=response.status_code)
     # Transfer headers from API response to Flask response
@@ -63,23 +68,21 @@ def round_robin_worker_call(endpoint):
 
 @app.route('/api/v1/hello')
 def hello():
+    # Route for handling hello endpoint using round-robin
     return round_robin_worker_call('/api/v1/hello')
-
 
 @app.route('/worker/stats')
 def worker_stats():
+    # Route for handling worker statistics endpoint using round-robin
     return round_robin_worker_call('/worker/stats')
-
-
 
 # -----------------------------------------------CONFIG MANAGER------------------------------------
 
-
 @app.route("/")
 def home():
+    # Load and render the configuration data on the home page
     data = load_config()
     return render_template('home.html', data=data)
-
 
 @app.route('/config', methods=['POST'])
 def configChange():
@@ -90,17 +93,17 @@ def configChange():
         "average_delay":      b if b != "" else cur_data["average_delay"],
         "failure_percentage": c if c != "" else cur_data["failure_percentage"]
     }
+    # Save new configuration data, stop and restart worker servers
     save_config(data)
     load_balancer.stop_server()
     load_balancer.start_server()
     return redirect("/")
 
-
-
-
-
 # -----------------------------------------------MAIN------------------------------------
+
 if __name__ == '__main__':
+    # Start the load balancer and Flask app
     load_balancer.start_server()
     app.run(port=3000)
+    # Stop the load balancer when the app is terminated
     load_balancer.stop_server()
